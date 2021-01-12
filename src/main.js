1edafb37ea8f5111a9014bef11d27d5d8c02d533
Fabian Siegel <fabians1999@gmail.com>
2021-01-12 19:50:50 +0100

    eslint proper config

--- src/main.js
+++ src/main.js
@@ -1,37 +1,37 @@
-import Instances from "./Instances.hbs";
-import "./main.scss";
+import Instances from './Instances.hbs';
+import './main.scss';
 
-const owncast_instances = [
-    "https://watch.owncast.online/",
-    "https://tv.grindhold.de/"
+const owncastInstances = [
+	'https://watch.owncast.online/',
+	'https://tv.grindhold.de/'
 
 ];
 
 window.addEventListener('load', function (event) {
-    const root = document.getElementById("app-root");
-    let instances = [];
-    root.innerHTML = Instances({
-        instances: instances,
-    });
+	const root = document.getElementById('app-root');
+	let instances = [];
+	root.innerHTML = Instances({
+		instances: instances,
+	});
 
-    for (const instance of owncast_instances) {
-        fetch(instance + "api/yp")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
+	for (const instance of owncastInstances) {
+		fetch(instance + 'api/yp')
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
 
-                const processed = {
-                    "name": data.name,
-                    "description": data.description,
-                    "viewer": data.viewerCount,
-                    "thumbnail": instance+"thumbnail.jpg",
-                    "status": data.online ? "online" : "offline",
-                };
+				const processed = {
+					'name': data.name,
+					'description': data.description,
+					'viewer': data.viewerCount,
+					'thumbnail': instance + 'thumbnail.jpg',
+					'status': data.online ? 'online' : 'offline',
+				};
 
-                instances = [...instances, processed];
-                root.innerHTML = Instances({
-                    instances: instances,
-                });
-            });
-    }
+				instances = [...instances, processed];
+				root.innerHTML = Instances({
+					instances: instances,
+				});
+			});
+	}
 });
