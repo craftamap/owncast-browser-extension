76050343ca79e7366899d91b7a1a38268777248a
Fabian Siegel <fabians1999@gmail.com>
2021-12-21 09:33:55 +0100

    options: add validator for export using json schema

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
+  <div class="options-wrapper">
+    <h1>Options</h1>
     <form
       id="options"
       @submit="store"
+      @reset="reset"
     >
-      <h1>
+      <h2>
         Pop-Up and Notifications
-      </h1>
+      </h2>
       <div class="form-section">
         <label>
           <div>pop-up theme</div>
@@ -68,9 +70,9 @@
           >
         </label>
       </div>
-      <h1>
+      <h2>
         Automatic Username
-      </h1>
+      </h2>
       <div class="form-section">
         <label>
           <div>your username</div>
@@ -85,10 +87,16 @@
           >
         </label>
       </div>
-      <div class="flex items-center">
+      <div>
         <input
+          :disabled="!isDirty"
           type="submit"
           value="Save"
+        >
+        <input
+          :disabled="!isDirty"
+          type="reset"
+          value="Reset Changes"
         >
         <LoadingIcon
           v-if="displayLoading"
@@ -101,24 +109,32 @@
         />
       </div>
     </form>
+    <h1>Export & Import</h1>
+    <p>The extension allows you to export your current options as well as your subscribed instances and re-import them.</p>
     <div>
-      <div class="flex items-center">
+      <div class="export">
         <input
+          :disabled="isDirty"
+          :title="isDirty ? 'you need to save your options first in order to export.' : ''"
           type="button"
           value="Export"
           @click="triggerExport"
         >
       </div>
-      <div class="flex items-center">
+      <div class="import">
         <input
           ref="importFile"
           type="file"
         >
         <input
+          :disabled="isDirty"
           type="button"
           value="Import"
           @click="triggerImport"
         >
+      </div>
+      <div>
+        {{ importExportErrors }}
       </div>
     </div>
   </div>
@@ -130,7 +146,6 @@ import ErrorIcon from '../shared/components/icons/ErrorIcon.vue'
 import SuccessIcon from '../shared/components/icons/SuccessIcon.vue'
 
 export default {
-
 	name: 'Options',
 	components: {
 		LoadingIcon,
@@ -147,12 +162,19 @@ export default {
 		displaySuccess () {
 			return this.$store.state.display.success
 		},
+		importExportErrors () {
+			return this.$store.state.display.importExportErrors
+		},
+		isDirty () {
+			return this.$store.state.display.dirty
+		},
 		badge: {
 			get () {
 				return this.$store.state.options.badge
 			},
 			set (value) {
 				this.$store.commit('setBadge', value)
+				this.$store.commit('setDirty', true)
 			},
 		},
 		notifications: {
@@ -161,6 +183,7 @@ export default {
 			},
 			set (value) {
 				this.$store.commit('setNotifications', value)
+				this.$store.commit('setDirty', true)
 			},
 		},
 		interval: {
@@ -168,7 +191,8 @@ export default {
 				return this.$store.state.options.interval
 			},
 			set (value) {
-				this.$store.commit('setInterval', value)
+				this.$store.commit('setInterval', Number(value))
+				this.$store.commit('setDirty', true)
 			},
 		},
 		username: {
@@ -177,6 +201,7 @@ export default {
 			},
 			set (value) {
 				this.$store.commit('setUsername', value)
+				this.$store.commit('setDirty', true)
 			},
 		},
 		theme: {
@@ -185,6 +210,7 @@ export default {
 			},
 			set (value) {
 				this.$store.commit('setTheme', value)
+				this.$store.commit('setDirty', true)
 			},
 		},
 		layout: {
@@ -193,6 +219,7 @@ export default {
 			},
 			set (value) {
 				this.$store.commit('setLayout', value)
+				this.$store.commit('setDirty', true)
 			},
 		},
 	},
@@ -200,6 +227,10 @@ export default {
 		async store (e) {
 			e.preventDefault()
 			return this.$store.dispatch('storeOptionsInStorage')
+		},
+		async reset (e) {
+			e.preventDefault()
+			return this.$store.dispatch('getOptionsFromStorage')
 		},
 		async triggerExport (e) {
 			e.preventDefault()
@@ -221,48 +252,72 @@ export default {
 @import "../scss/colors.scss";
 
 html, body {
-  min-height: 200px;
-  min-width: 200px;
+	min-height: 200px;
+	min-width: 200px;
 }
 
-form {
-  font-size: 1rem;
-  line-height: 1.5rem;
-  margin: 2rem;
+.options-wrapper {
+	font-size: 1rem;
+	line-height: 1.5rem;
+	margin: 2rem;
 }
 
 input[type='checkbox'] + label {
-  margin-left: 1rem;
+	margin-left: 1rem;
 }
 
-input[type='submit'] {
-  background-color: $blue-600;
-  padding: 0.5rem 0;
-  width: 8rem;
-  margin: 0.5rem 0.5rem 0.5rem 0;
-  border-radius: 0.25rem;
-  color: white;
+input[type='submit'],input[type='button'],input[type='reset'] {
+	padding: 0.5rem 0;
+	width: 8rem;
+	margin: 0.5rem 0.5rem 0.5rem 0;
+	border-radius: 0.25rem;
 
-  &:active {
-    background-color: $blue-800;
-  }
+	&[type='submit'] {
+		background-color: $blue-600;
+		color: white;
+		&:disabled {
+			background-color: $blue-400;
+			color: gray;
+		}
+
+		&:active {
+			background-color: $blue-800;
+		}
+	}
 }
 
 h1 {
-  font-size: 1.125rem;
-  line-height: 1.75rem;
-  font-weight: 700;
+	font-size: 1.25rem;
+	line-height: 2rem;
+	font-weight: 700;
+}
+
+h2 {
+	font-size: 1.125rem;
+	line-height: 1.75rem;
+	font-weight: 700;
 }
 
 .form-section {
-  display: flex;
-  align-items:center;
-  margin-bottom: 0.5rem;
+	display: flex;
+	align-items:center;
+	margin-bottom: 0.5rem;
 }
 
 .description {
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-  color: $gray-500;
+	font-size: 0.875rem;
+	line-height: 1.25rem;
+	color: $gray-500;
+}
+
+.export {
+	display: flex;
+	justify-content: flex-end;
+}
+
+.import {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 </style>
--- src/options/store/index.js
+++ src/options/store/index.js
@@ -1,16 +1,25 @@
 /**
+ * @typedef Theme
+ * @type {"dark"|"light"}
+ * @typedef Layout
+ * @type {"normal"|"compact"}
+ *
  * @typedef Options
  * @type {object}
  * @prop {boolean} badge
  * @prop {boolean} notifications
  * @prop {number}  interval
  * @prop {string}  username
+ * @prop {Theme}   theme
+ * @prop {Layout}  layout
  *
  * @typedef Display
  * @type {object}
  * @prop {boolean} loading
  * @prop {boolean} success
  * @prop {boolean} error
+ * @prop {boolean} dirty
+ * @prop {string} importExportErrors
  *
  * @typedef State
  * @type {object}
@@ -32,12 +41,16 @@ export default new Vuex.Store({
 			loading: false,
 			success: false,
 			error: false,
+			dirty: false,
+			importExportErrors: '',
 		},
 		options: {
 			badge: false,
 			notifications: false,
 			interval: 0,
 			username: '',
+			theme: '',
+			layout: '',
 		},
 		instances: [],
 	},
@@ -62,29 +75,42 @@ export default new Vuex.Store({
 			state.display.success = false
 			state.display.error = false
 		},
+		setDirty (state, dirtyness) {
+			state.display.dirty = dirtyness
+		},
+		setImportExportErrors (state, errors) {
+			state.display.importExportErrors = errors
+		},
 		setOptions (state, options) {
 			state.options = options
 			console.log('[setOptions]', state.options)
 		},
+		/** @param {Array<string>} instances */
 		setInstances (state, instances) {
 			state.instances = instances
 			console.log('[setInstances]', state.instances)
 		},
+		/** @param {boolean} badge */
 		setBadge (state, badge) {
 			state.options.badge = badge
 		},
+		/** @param {boolean} notifications */
 		setNotifications (state, notifications) {
 			state.options.notifications = notifications
 		},
+		/** @param {number} interval */
 		setInterval (state, interval) {
 			state.options.interval = interval
 		},
+		/** @param {string} username */
 		setUsername (state, username) {
 			state.options.username = username
 		},
+		/** @param {Theme} theme */
 		setTheme (state, theme) {
 			state.options.theme = theme
 		},
+		/** @param {Layout} layout */
 		setLayout (state, layout) {
 			state.options.layout = layout
 		},
@@ -103,7 +129,7 @@ export default new Vuex.Store({
 				type: 'getInstances',
 			}).then((instances) => {
 				console.log('[getInstancesFromStorage]', instances)
-				commit('setInstances', instances)
+				commit('setInstances', instances.instances)
 			})
 		},
 		async storeOptionsInStorage ({ commit, dispatch, state }) {
@@ -120,6 +146,8 @@ export default new Vuex.Store({
 				return dispatch('getOptionsFromStorage')
 			}).then(() => {
 				return commit('setDisplaySuccess')
+			}).then(() => {
+				return commit('setDirty', false)
 			}).catch(() => {
 				return commit('setDisplayError')
 			}).then(() => {
@@ -135,17 +163,35 @@ export default new Vuex.Store({
 				const exportDataBlob = generateExport(state)
 				const url = URL.createObjectURL(exportDataBlob)
 				const link = document.createElement('a')
-				link.download = 'owncast-browser-extension-export.json'
+				const date = new Date()
+				link.download = `owncast-browser-extension-export-${date.toISOString().replaceAll(':', '-')}.json`
 				link.href = url
 				link.click()
 				URL.revokeObjectURL(link.href)
+				document.removeChild(link)
 
 				window.open(url, '_blank')
 			})
 		},
-		async triggerImport ({ dispatch }, file) {
-			const parsedExport = await parseExport(file)
-			console.log(parsedExport)
+		async triggerImport ({ commit, dispatch, state }, file) {
+			commit('setImportExportErrors', '')
+			try {
+				const parsedExport = await parseExport(file)
+				// for now, let's assume a export only contains existing owncast instances.
+				parsedExport.instances.forEach((instance) => {
+					browser.runtime.sendMessage({
+						type: 'addInstanceInStorage',
+						data: {
+							url: instance,
+						},
+					})
+				})
+				// Let's merge current setting with the imported ones
+				commit('setOptions', { ...state.options, ...parsedExport.options })
+				dispatch('storeOptionsInStorage')
+			} catch (e) {
+				commit('setImportExportErrors', e)
+			}
 		},
 	},
 })
